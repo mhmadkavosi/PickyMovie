8b387e5c03e212d3ad00316a812a4f9ce2de88ce
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2021-01-01 16:16:48 +0330

    genra and release date

--- src/server/scrapers/imdb/getMovie.js
+++ src/server/scrapers/imdb/getMovie.js
@@ -23,9 +23,27 @@ const MOVIE_ID = `tt7137846`;
     const ratingCount = document.querySelector('span[itemprop="ratingCount"]')
       .innerText;
 
+    let genra = [];
+    const genras = document.querySelectorAll('.subtext > a');
+    [...genras].forEach((el) => {
+      genra.push(el.innerText);
   });
+    const releaseDate = [...genra].pop();
+    genra = genra.slice(0, genra.length - 1);
 
+    /* Returning an object filled with the scraped data */
     return {
       title,
+      genra,
+      rating,
+      ratingCount,
+      timeWatch,
+      releaseDate,
     };
+  });
+
+  /* Outputting what we scraped */
+  console.log(data);
+
+  await browser.close();
 })();
