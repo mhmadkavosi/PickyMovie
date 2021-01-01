ac0479819a6986834f68d4e33180dfe2f90d50c8
Mohammad Kavosi <mhmad.kavosi@gmail.com>
2021-01-01 16:15:07 +0330

    get title of movie

---
+++ src/server/scrapers/imdb/getMovie.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+
+const IMDB_URL = (movie_id) => `https://www.imdb.com/title/${movie_id}/`;
+const MOVIE_ID = `tt7137846`;
+
+(async () => {
+  /* Initiate the Puppeteer browser */
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  /* Go to the IMDB Movie page and wait for it to load */
+  await page.goto(IMDB_URL(MOVIE_ID), { waitUntil: 'networkidle0' });
+
+  /* Run javascript inside of the page */
+  const data = await page.evaluate(() => {
+    const title = document.querySelector('div[class="title_wrapper"] > h1')
+      .innerText;
+  });
+
+    return {
+      title,
+    };
+})();
